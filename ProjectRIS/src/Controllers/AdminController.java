9412da78f57cca910722b88073d099c6cdfaad5c
krcafferty01 <98491243+krcafferty01@users.noreply.github.com>
2022-03-24 11:23:21 -0400

    Update AdminController.java
    add newOrder view link newOrder method

--- ProjectRIS/src/Controllers/AdminController.java
+++ ProjectRIS/src/Controllers/AdminController.java
@@ -35,6 +35,12 @@ public class AdminController {
 	private DatePicker DOB;
 	@FXML 
 	ChoiceBox<String> sexChoiceBox;
+	@FXML
+	private TextField OrderReferralMD;
+	@FXML
+	private TextField OrderModalityNeeded;
+	@FXML
+	private TextArea OrderNotes;
 
 	
 	public void userLogOut(ActionEvent event) throws IOException {
@@ -84,8 +90,52 @@ public class AdminController {
 		m.changeScene("../Views/NewPatient.fxml");
 	}
 	
+	public void NewOrderButton(ActionEvent event) throws IOException{
+		
+		Main m = new Main();
+		m.changeScene("../Views/NewOrder.fxml");
+	}
 	
 	
+	public void addNewOrder(ActionEvent event) throws IOException {
+		 Connection conn = null;
+	      Statement stmt = null;
+	      
+	      
+	      try {
+	          try {
+	             Class.forName("com.mysql.cj.jdbc.Driver");
+	          } catch (Exception e) {
+	             System.out.println(e);
+	       }
+	       conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ris", "root", "Brikev34$");
+	       System.out.println("Connection is created successfully:");
+	       stmt = (Statement) conn.createStatement();
+	       
+	       
+	       String query1 = "INSERT INTO orders(patient,referral_md,modality,notes) VALUES  ('"+PatientFirstName.getText()+" "+PatientLastName.getText()+"', '"+OrderReferralMD.getText()+"', '"+OrderModalityNeeded.getText()+"', '"+OrderNotes.getText()+"')";
+	       System.out.println(query1);
+	       stmt.executeUpdate(query1);
+	       System.out.println("Record is inserted in the table successfully..................");
+	       } catch (SQLException excep) {
+	          excep.printStackTrace();
+	       } catch (Exception excep) {
+	          excep.printStackTrace();
+	       } finally {
+	          try {
+	             if (stmt != null)
+	                conn.close();
+	          } catch (SQLException se) {}
+	          try {
+	             if (conn != null)
+	                conn.close();
+	          } catch (SQLException se) {
+	             se.printStackTrace();
+	          }  
+	       }
+	       System.out.println("Please check it in the MySQL Table......... â€¦â€¦..");
+	       OrderButton(event);
+	      }
 	
 	public void addNewPatient(ActionEvent event) throws IOException {
 		
